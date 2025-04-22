ea415fbae7487d1d31eea90f33bfc56e8d46ae0b
Sanjida Akter <sanjidatanha09@gmail.com>
2025-04-22 10:46:36 +0600

    url done

---
+++ src/app/add-task/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>Add Task Component</h1>
+            
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/show-task/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>Show task page</h1>
+        </div>
+    );
+};
+
+export default page;
