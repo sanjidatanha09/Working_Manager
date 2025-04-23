a612430dab98cd05f4313b3874d75aed47aed55b
Sanjida Akter <sanjidatanha09@gmail.com>
2025-04-23 12:56:44 +0600

    hi

---
+++ src/app/services/userService.js
@@ -0,0 +1,5 @@
+import { httpAxios } from "@/helper/httpHelper";
+
+export async function addUser(user) {
+    const result = await httpAxios.post("/api/users", user).then((response) => response.data);
+}
