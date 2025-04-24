6b5f02df2fd764ef59c6d7be5975b442295d542c
Sanjida Akter <sanjidatanha09@gmail.com>
2025-04-24 22:48:41 +0600

    dfdf

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,13 @@
-async function takeTime() {
-  await new Promise((resvole) => {
+// async function takeTime() {
+//   await new Promise((resvole) => {
     
-    setTimeout(resvole,3000);
-  });
-}
+//     setTimeout(resvole,3000);
+//   });
+// }
 
 
-export default async function About() {
-  await takeTime();
-  throw new Error("This is manual errr");
-  return <h1>About Page</h1>;
-}
+// export default async function About() {
+//   await takeTime();
+//   throw new Error("This is manual errr");
+//   return <h1>About Page</h1>;
+// }
