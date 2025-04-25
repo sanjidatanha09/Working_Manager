69816dedaa03b3c36e6bbf3f79780169159d2b0a
Sanjida Akter <sanjidatanha09@gmail.com>
2025-04-25 08:00:53 +0600

    dfdfdf

--- src/app/myprofile/user/page.js
+++ src/app/myprofile/user/page.js
@@ -1,5 +1,7 @@
 export default function UserProfile(){
     return (
-        <h1>myt ne user s profile</h1>
+        <h1>myt ne user s profile
+            <h1>myt ne user s profile</h1>
+        </h1>
     );
 }
