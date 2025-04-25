94f227efc1a72752efe99d0cc31df67713f053a1
Sanjida Akter <sanjidatanha09@gmail.com>
2025-04-25 08:00:10 +0600

    dfdf

--- src/Components/CustomNavbar.jsx
+++ src/Components/CustomNavbar.jsx
@@ -8,7 +8,7 @@ const CustomNavbar = () => {
     <nav className="bg-blue-600 h-12 py-2 px-3 ">
       <div className="grid grid-cols-3 justify-between">
         <div>
-          <h1>logo</h1>
+          <h1>logo and task manager</h1>
         </div>
         <div className="flex items-center justify-between gap-4 font-bold">
           <Link href="/">Home</Link>
